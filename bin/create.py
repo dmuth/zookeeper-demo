53867db81593275b419c062056aa42b00643d9f7
Douglas Muth <dmuth@dmuth.org>
2014-12-21 21:10:13 -0500

    Now sleeping for a random number of seconds

--- bin/create.py
+++ bin/create.py
@@ -8,6 +8,7 @@ import commands
 import json
 import logging
 import os
+import random
 import signal
 import sys
 import time
@@ -15,20 +16,13 @@ import time
 
 import core
 
+
 #
 # Return the current IP address
 #
 def getIP():
 	retval = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
 	return(retval)
-
-#
-# Bails out when we get a signal
-#
-def signal_handler(signal, frame):
-	logging.info("Ctrl-C received. Deleting key")
-	zk.delete(key)
-	sys.exit(0)
 
 
 zk = core.connect()
@@ -40,13 +34,15 @@ data["pid"] = os.getpid()
 key = zk.create(core.key + "/testseq-", json.dumps(data), ephemeral=True, sequence=True)
 logging.info("Inserted IP and PID into key '%s'" % key)
 
-logging.info("Just hanging out because these are emphemeral keys. Press ^C to exit...")
-
 #
-# Wait for the user to press ctrl-C
+# Sleep for a random number of seconds
 #
-signal.signal(signal.SIGINT, signal_handler)
-signal.pause()
+num_secs = random.randint(5, 60)
 
+logging.info("Sleeping for %d seconds..." % num_secs)
+time.sleep(num_secs)
+
+logging.info("Woke up! Deleting our key and disconnecting.")
+zk.delete(key)
 
 
