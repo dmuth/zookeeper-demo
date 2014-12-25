eda029be38ffe66fa148a2a7a894c0fe2f94d969
Douglas Muth <dmuth@dmuth.org>
2014-12-25 18:12:52 -0500

    Added installation of various software and Pip packages.

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
+#
+# Provision our Vagrant instance
+#
+
+
+#
+# Errors are fatal
+#
+set -e
+
 
 #
 # Create a symlink to /vagrant
 #
 ln -sf /vagrant .
 
+
+MINUTES=1440
+#MINUTES=10 # Debugging
+#MINUTES=1 # Debugging
+FOUND=`find /var/apt-get-update -mmin +${MINUTES}`
+if test "$FOUND"
+then
+	echo "# "
+	echo "# apt-get update hasn't been run in ${MINUTES} minutes. Running."
+	echo "# "
+	apt-get update
+	touch /var/apt-get-update
+fi
+
+
+apt-get install -y python-setuptools zookeeper
+easy_install pip
+pip install -r /vagrant/requirements.txt
+
+/usr/share/zookeeper/bin/zkServer.sh start
 
