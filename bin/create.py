828780a96a8357890869ecae26d6f22f21d0747b
Douglas Muth <dmuth@dmuth.org>
2014-12-28 17:41:00 -0500

    Added "seconds" argument.

--- bin/create.py
+++ bin/create.py
@@ -2,6 +2,15 @@
 #
 # This script creates Ephemeral sequenced zodes
 #
+"""
+Usage: create.py [SECONDS]
+
+	-h, --help		Show this help section
+	SECONDS			How many seconds to stay running for?
+					If this is not specified, a random value is used
+
+
+"""
 
 
 import commands
@@ -13,6 +22,7 @@ import signal
 import sys
 import time
 
+from docopt import docopt
 
 import core
 
@@ -24,6 +34,8 @@ def getIP():
 	retval = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
 	return(retval)
 
+params = docopt(__doc__, version='0.1.0 alpha')
+
 
 zk = core.connect()
 data = {}
@@ -34,10 +46,9 @@ data["pid"] = os.getpid()
 key = zk.create(core.key + "/testseq-", json.dumps(data), ephemeral=True, sequence=True)
 logging.info("Inserted IP and PID into key '%s'" % key)
 
-#
-# Sleep for a random number of seconds
-#
-num_secs = random.randint(5, 60)
+num_secs = int(params["SECONDS"])
+if (not num_secs):
+	num_secs = random.randint(5, 60)
 
 logging.info("Sleeping for %d seconds..." % num_secs)
 time.sleep(num_secs)
