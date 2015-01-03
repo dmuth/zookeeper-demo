68bf2e198c6258a32caa34193d3c13ae3851e076
Douglas Muth <dmuth@dmuth.org>
2015-01-02 21:48:18 -0500

    Now checking to see if the apt-get-update file exists before running find.

--- bin/provision.sh
+++ bin/provision.sh
@@ -9,24 +9,30 @@
 #
 set -e
 
-
 MINUTES=1440
 #MINUTES=10 # Debugging
 #MINUTES=1 # Debugging
-FOUND=`find /var/apt-get-update -mmin +${MINUTES}`
-if test "$FOUND"
+
+
+FILE="/var/apt-get-update"
+if test -f ${FILE}
 then
-	echo "# "
-	echo "# apt-get update hasn't been run in ${MINUTES} minutes. Running."
-	echo "# "
-	apt-get update
-	touch /var/apt-get-update
+
+	FOUND=`find ${FILE} -mmin +${MINUTES}`
+	if test "$FOUND"
+	then
+		echo "# "
+		echo "# apt-get update hasn't been run in ${MINUTES} minutes. Running."
+		echo "# "
+		apt-get update
+		touch ${FILE}
+
+	fi
+
 fi
 
 
 apt-get install -y python-setuptools zookeeper
 easy_install pip
 pip install -r /vagrant/requirements.txt
-
-/usr/share/zookeeper/bin/zkServer.sh start
 
